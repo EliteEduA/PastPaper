8cc4bac4b897c2e2882f9117a92d062d76738a77
EliteEduA <64740099+EliteEduA@users.noreply.github.com>
2020-05-04 03:13:47 +1200

    Add files via upload

---
+++ Document/MathNote/Housekeep.bat
@@ -0,0 +1,57 @@
+del /q *.aux
+del /q *.bbl
+del /q *.bcf
+del /q *.bib
+del /q *.blg
+del /q *.idx
+del /q *.ilg
+del /q *.ist
+del /q *.ind
+del /q *.log
+
+del /q *.out
+del /q *.ptc
+del /q *.run.xml
+del /q *.toc
+del /q *.xsim
+
+del /q *-exercise-body.tex
+del /q *-solution-body.tex
+
+
+
+del /q math\*.aux
+del /q math\*.bbl
+del /q math\*.bcf
+del /q math\*.bib
+del /q math\*.blg
+del /q math\*.idx
+del /q math\*.ilg
+del /q math\*.ind
+del /q math\*.log
+
+del /q math\*.ptc
+del /q math\*.run.xml
+del /q math\*.toc
+del /q math\*.xsim
+
+
+del /q math\book\*.aux
+del /q math\book\*.bbl
+del /q math\book\*.bcf
+del /q math\book\*.bib
+del /q math\book\*.blg
+del /q math\book\*.idx
+del /q math\book\*.ilg
+del /q math\book\*.ist
+del /q math\book\*.ind
+del /q math\book\*.log
+
+del /q math\book\*.out
+del /q math\book\*.ptc
+del /q math\book\*.run.xml
+del /q math\book\*.toc
+del /q math\book\*.xsim
+
+del /q math\book\*-exercise-body.tex
+del /q math\book\*-solution-body.tex
